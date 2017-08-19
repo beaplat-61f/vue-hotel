9583f74acdd5b56bf5c415a4b09ee7b348375552
Boby-Co <948515960@qq.com>
2017-08-19 20:40:01 +0800

    add calendar

---
+++ src/page/book/SelectCalendar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'select-calendar',
+    data () {
+      return {
+        headerTitle: '请选入住日期'
+      }
+    },
+    methods: {
+      ...mapMutations ({
+        updateHeader: 'UPDATE_HEADER',
+        updateRoom: 'UPDATE_ROOM'
+      }),
+      initHeader () {
+        this.updateHeader ({
+          title: this.headerTitle,
+          topTitle: '维也纳酒店详情',
+          showRight: false
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
