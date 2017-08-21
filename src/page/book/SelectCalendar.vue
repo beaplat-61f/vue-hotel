e747e94fd49f8df21f54734b96537c7fe76470d5
Boby-Co <948515960@qq.com>
2017-08-21 22:20:21 +0800

    update calender

--- src/page/book/SelectCalendar.vue
+++ src/page/book/SelectCalendar.vue
@@ -1,95 +1,82 @@
 <template>
   <div id="select-calendar">
-    <transition name="fade">
-      <div class="calendar-dialog" v-if="calendarFilter.show">
-        <div class="calendar-dialog-mask" @click="closeByDialog"></div>
-
-        <div class="calendar-dialog-body">
-          <calendar ref="calendar" :range="calendarFilter.range" :zero="calendarFilter.zero" :lunar="calendarFilter.lunar" :value="calendarFilter.value"
-                    @select="calendarFilter.select"></calendar>
-        </div>
-
+    <div class="in">
+      <div class="tip">
+        <span><strong v-text="$route.params.type+tipDate"></strong></span>
       </div>
-    </transition>
+      <inline-calendar :render-month="renderMonth" :start-date="startDateFilter" disable-past
+                       highlight-weekend :show-last-month="false"
+                       :show-next-month="false" :weeks-list="weeksList" @on-change="changeInDate"></inline-calendar>
+    </div>
   </div>
 </template>
 
 <script>
-  import {dateFormat} from 'vux'
-  import {mapMutations} from 'vuex'
-  import calendar from '../../components/calendar/calendar.vue'
+  import {dateFormat, InlineCalendar} from 'vux'
+  import {mapMutations, mapState} from 'vuex'
 
   export default {
     name: 'select-calendar',
     components: {
-      calendar
+      InlineCalendar
+    },
+    computed: {
+      startDateFilter() {
+        let startDate = this.order.begin.getTime()+(1000*60*60*24)
+        return this.$route.params.type == '入住' ? dateFormat(new Date(), 'YYYY-MM-DD') : dateFormat(new Date(startDate), 'YYYY-MM-DD')
+      },
+      ...mapState({
+        order: state => state.order
+      }),
+      tipDate() {
+        return this.$route.params.type == '入住' ? dateFormat(this.order.begin, 'YYYY月MM日') : dateFormat(this.order.end, 'YYYY月MM日')
+      },
+      renderMonth() {
+        let month = this.$route.params.type == '入住' ? dateFormat(this.order.begin, 'YYYY-M').split('-') : dateFormat(this.order.end, 'YYYY-M').split('-')
+        return [parseInt(month[0]), parseInt(month[1])]
+      }
     },
     data () {
       return {
-        headerTitle: '请选入住日期',
-        calendar: {
-          show: true,
-          range: true,
-          zero: true,
-          value: [[2018, 8, 19], [2018, 8, 20]], //默认日期
-          lunar: true, //显示农历
-          select: (begin, end) => {
-//            console.log (begin, end, new Date(begin), new Date(end), new Date(end).getDate()-new Date(begin).getDate())
-            let beginDate = new Date(begin),
-                endDate = new Date(end),
-                now = new Date(dateFormat(new Date(), 'YYYY/MM/DD').split('/')),
-                nextDay = new Date(dateFormat((new Date()).setDate(now.getDate()+1), 'YYYY/MM/DD').split('/'))
-//            debugger
-
-            if (beginDate.getTime()<now.getTime()) {
-              beginDate = now
-            }
-
-            if (endDate.getTime()<nextDay.getTime()) {
-              endDate = nextDay
-            }
-
-            this.updateOrder({
-              begin: beginDate,
-              end: endDate,
-              total_day: (endDate.getTime()-beginDate.getTime())/(24*60*60*1000)
-            })
-
-            let beginStr = dateFormat(beginDate, 'YYYY/MM/DD'),
-                endStr = dateFormat(endDate, 'YYYY/MM/DD'),
-                beginArr = beginStr.split('/'),
-                endArr = endStr.split('/')
-
-            this.calendar.value = [beginArr, endArr]
-            console.log(this.$refs.calendar.days)
-            this.$router.back()
-          }
-        }
-      }
-    },
-    computed: {
-      calendarFilter() {
-        let now = new Date(),
-            nextDay = new Date((new Date()).setDate(now.getDate()+1))
-        this.calendar.value = [dateFormat(now, 'YYYY/MM/DD').split('/'), dateFormat(nextDay, 'YYYY/MM/DD').split('/')]
-        return this.calendar
+        weeksList: ['日', '一', '二', '三', '四', '五', '六']
       }
     },
     methods: {
       ...mapMutations ({
         updateHeader: 'UPDATE_HEADER',
-        updateRoom: 'UPDATE_ROOM',
         updateOrder: 'UPDATE_ORDER'
       }),
       initHeader () {
         this.updateHeader ({
-          title: this.headerTitle,
+          title: '请选'+this.$route.params.type+'日期',
           topTitle: '维也纳酒店详情',
           showRight: false
         })
       },
-      closeByDialog(){
-        this.calendar.show=false;
+      changeInDate(val) {
+//        debugger
+        let beginDate = null,
+            endDate = null
+        if (this.$route.params.type == '入住') {
+          let endStr = dateFormat(this.order.end, 'YYYY-MM-DD')
+          let next = new Date(val).getTime()+(1000*60*60*24)
+          beginDate = new Date(val)
+          endDate = (endStr<=val ? new Date(next) : this.order.end)
+        } else {
+          console.log(val, new Date(val))
+          beginDate = this.order.begin
+          endDate = new Date(val)
+        }
+
+        beginDate = new Date(dateFormat(beginDate, 'YYYY/MM/DD'))
+        endDate = new Date(dateFormat(endDate, 'YYYY/MM/DD'))
+        this.updateOrder({
+          begin: beginDate,
+          end: endDate,
+          total_day: (endDate.getTime()-beginDate.getTime())/(24*60*60*1000)
+        })
+
+        this.$router.replace('/')
       }
     },
     mounted () {
@@ -99,5 +86,16 @@
 </script>
 
 <style scoped>
+  * {
+    font-size: .7rem;
+    padding: 0;
+    margin: 0;
+  }
 
+  .tip {
+    height: 1.8rem;
+    line-height: 1.8rem;
+    background: #EDEDED;
+    text-align: center;
+  }
 </style>
